1b0a7e36c6f61a1b7f168cb4c5a6d3ee757f47ad
Alisa Kocheva <alisa.ko4eva@gmail.com>
2026-03-05 13:54:25 +0700

    add commandParser + commands

--- Sources/CrystalsAndDragons/Controller/Commands/Command.swift
+++ Sources/CrystalsAndDragons/Controller/Commands/Command.swift
@@ -4,3 +4,14 @@
 //
 //  Created by Алиса on 04/03/26.
 //
+
+import Foundation
+
+enum Command {
+    case move(Direction)
+    case get(itemName: String, color: Color)
+    case drop(itemName: String, color: Color)
+    case open(color: Color)
+    case inventory
+    case quit
+}
--- Sources/CrystalsAndDragons/Controller/Commands/CommandParser.swift
+++ Sources/CrystalsAndDragons/Controller/Commands/CommandParser.swift
@@ -7,3 +7,108 @@
 
 import Foundation
 
+struct CommandParser {
+    func parse(command: String) -> Command? {
+        let tokens = normalizeTokens(input: command)
+        
+        if tokens.contains("get") {
+            
+            guard let key = keyItem(input: tokens) else {
+                return nil
+            }
+            
+            return .get(itemName: key.0, color: key.1)
+        }
+        
+        if tokens.contains("drop") {
+            
+            guard let key = keyItem(input: tokens) else {
+                return nil
+            }
+            
+            return .drop(itemName: key.0, color: key.1)
+        }
+        
+        if tokens.contains("inventory") {
+            return .inventory
+        }
+        
+        
+        if tokens.contains("quit") {
+            return .quit
+        }
+        
+        if tokens.contains("open") {
+            guard let itemColor = parseColor(input: tokens) else {
+                return nil
+            }
+            return .open(color: itemColor)
+        }
+        
+        guard let dir = parseDirection(input: tokens) else {
+            return nil
+        }
+        return .move(dir)
+    }
+    
+    
+    
+    private func normalizeTokens(input: String) -> [String] {
+          return input
+              .lowercased()
+              .replacingOccurrences(of: ",", with: " ")
+              .replacingOccurrences(of: ".", with: " ")
+              .split(whereSeparator: { $0.isWhitespace })
+              .map(String.init)
+    }
+    
+    private func parseColor(input: [String]) -> Color? {
+        var itemColor: Color? = nil
+        
+        if input.contains("blue") {
+            itemColor = .blue
+        }
+        
+        if input.contains("red") {
+            itemColor = .red
+        }
+        
+        if input.contains("yellow") {
+            itemColor = .yellow
+        }
+        
+        if input.contains("green") {
+            itemColor = .green
+        }
+        
+        return itemColor
+    }
+    
+    private func parseDirection(input: [String]) -> Direction? {
+        if input.contains("n") {
+            return .N
+        }
+        if input.contains("s") {
+            return .S
+        }
+        if input.contains("e") {
+            return .E
+        }
+        if input.contains("w") {
+            return .W
+        }
+        return nil
+    }
+    
+    private func keyItem(input: [String]) -> (String, Color)? {
+        guard input.contains("key") else {
+            return nil
+        }
+        let itemName = "key"
+        
+        guard let itemColor = parseColor(input: input) else {
+            return nil
+        }
+        return ("key", itemColor)
+    }
+}
